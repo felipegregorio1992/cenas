b3d2cd2ecfeb5b6501f91b332f4fa7cb8775e70a
felipe gregorio <felipegregorio1992@gmail.com>
2025-03-10 21:48:28 -0300

    Atualiza configurações do PostgreSQL com novos valores

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ echo "Configurando ambiente..."
 
 # Definir valores padrão para variáveis críticas
 export DB_CONNECTION=${DB_CONNECTION:-pgsql}
-export DB_HOST=${DB_HOST:-35.227.164.209}
-export DB_PORT=${DB_PORT:-6432}
+export DB_HOST=${DB_HOST:-dpg-ev7nv52r91rc739d1u2g-a}
+export DB_PORT=${DB_PORT:-5432}
 export DB_DATABASE=${DB_DATABASE:-linha_do_tempo}
 export DB_USERNAME=${DB_USERNAME:-linha_do_tempo_user}
-export DB_PASSWORD=${DB_PASSWORD:-455z58jML513WywHYCqLgsWHtPf40b11}
+export DB_PASSWORD=${DB_PASSWORD:-4E5z58jML5i3WywHYCqLgsWMiPI40Cji}
 
 # Verificar variáveis obrigatórias
 required_vars=(
